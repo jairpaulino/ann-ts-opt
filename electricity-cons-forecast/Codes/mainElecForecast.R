5a873f9832b466d2295b1a24652a2e72ced1cf4e
jairpaulino <63611561+jairpaulino@users.noreply.github.com>
2020-07-03 15:14:33 -0300

    Update mainElecForecast.R

--- electricity-cons-forecast/Codes/mainElecForecast.R
+++ electricity-cons-forecast/Codes/mainElecForecast.R
@@ -23,7 +23,6 @@ split.data = getSplitData(data$target, training_set_size = 0.8)
 normalized.data = getNormalizedData(split.data, lim_inf = 0.1, lim_sup = 0.9)
 
 # Phase 02 - Training phase (modelling) #####
-# Creating model 
 # Get optimal ARIMA and ANN models, respectively
 arima_model = getOptimalARIMA(normalized.data$training_set)
 ets_model = getOptimalETS(normalized.data$training_set)
@@ -36,38 +35,27 @@ onestep_ets = getETSForecasts(normalized.data$test_set, model = ets_model)
 onestep_nnar = getNNARForecasts(normalized.data$test_set, model = nnar_model)
 
 # Phase 04 - Performance analysis #####
-
 #Metrics
-metrics.table = as.data.frame(matrix(nrow = 3, ncol = 3))
-colnames(metrics.table) = c('ARIMA', 'ETS', 'NNAR')
-rownames(metrics.table) = c('MSE', 'MAPE', 'ARV')
+results.table = as.data.frame(matrix(nrow = length(normalized.data$test_set), 
+                                     ncol = 4))
+colnames(results.table) = c('test', 'ARIMA', 'ETS', 'NNAR')
+results.table$test = normalized.data$test_set
+results.table$ARIMA = onestep_arima
+results.table$ETS = onestep_ets
+results.table$NNAR = onestep_nnar
 
-# Getting MSE
-metrics.table$ARIMA[1] = getMSE(normalized.data$test_set, onestep_arima)
-
-metrics.table$ETS[1] = getMSE(normalized.data$test_set, onestep_ets)
-
-metrics.table$NNAR[1] = getMSE(normalized.data$test_set, onestep_nnar)
-
-# Getting MAPE
-metrics.table$ARIMA[2] = getMAPE(normalized.data$test_set, onestep_arima)
-
-metrics.table$ETS[2] = getMAPE(normalized.data$test_set, onestep_ets)
-
-metrics.table$NNAR[2] = getMAPE(normalized.data$test_set, onestep_nnar)
-
-# Getting Theil
-metrics.table$ARIMA[3] = getARV(normalized.data$test_set, onestep_arima)
-
-metrics.table$ETS[3] = getARV(normalized.data$test_set, onestep_ets)
-
-metrics.table$NNAR[3] = getARV(normalized.data$test_set, onestep_nnar)
-
+metrics.table = getCalculatedMetrics(results.table)
 View(metrics.table)
 
+# graphics
 plot.ts(normalized.data$test_set, lwd = 2)
 lines(onestep_arima, col = 2 , lwd = 2)
 lines(onestep_ets, col = 3 , lwd = 2)
 lines(onestep_nnar, col = 4 , lwd = 2)
 
+getMSE(normalized.data$test_set, onestep_nnar)
+getMAPE(normalized.data$test_set, onestep_nnar)
+getARV(normalized.data$test_set, onestep_nnar)
+
+# Optimization ####
 #matriz = getAnnMatrix(normalized.data$training_set, ar = 2, ss = 12, sar = 2)
